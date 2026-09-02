c2bc3dd0d55ed56bfe627f36304caa377437673f
paccach <andreypugach2288@gmail.com>
2026-09-02 11:03:20 +0300

    add task 5,6

---
+++ semester_1/lab1_introduction/tasks/6/task6/task6/task6.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int a, b, d;
+    cin >> a >> b >> d;
+    vector <int> pr;
+    pr.push_back(a);
+    int n = a;
+    while ((n += d) <= b)
+        pr.push_back(n);
+    for (int i = 0; i < pr.size(); i++)
+        if (pr[i] % 3 == 0)
+            cout << pr[i] << " ";
+}
