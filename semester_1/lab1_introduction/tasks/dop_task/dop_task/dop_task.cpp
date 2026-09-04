b04115b5a996d7194d8a1383096026dae76e09d2
paccach <andreypugach2288@gmail.com>
2026-09-04 20:45:35 +0300

    add dop task

---
+++ semester_1/lab1_introduction/tasks/dop_task/dop_task/dop_task.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    int kol = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (i == 1)
+            cout << "  ";
+        else
+            cout << "   ";
+        kol += 3;
+    }
+    for (int i = 1; i <= k; i++)
+    {
+        if (kol >= 20)
+        {
+            kol = 0;
+            cout << endl;
+        }
+        if (kol != 0)
+            cout << " ";
+        if (i / 10 == 0)
+            cout << " ";
+        cout << i;
+        kol += 3;
+    }
+    return 0;
+}
